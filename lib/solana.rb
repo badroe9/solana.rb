e920b9d123b94e43e00ff8b71e7da80e37f8811a
mvanderleest <morganv@hey.com>
2022-07-07 21:31:10 -0500

    gem creation

---
+++ lib/solana.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "solana/version"
+
+module Solana
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/solana/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Solana
+  VERSION = "0.1.0"
+end
